9fe3470771d830191cb155a4d9ed53f753e58949
David Arias <querquobad69@gmail.com>
2013-10-29 20:04:19 -0600

    Archivo index.php básico
    Signed-off-by: David Arias <querquobad69@gmail.com>

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Insert title here</title>
+	</head>
+		<?php
+
+		?>
+	<body>
+	</body>
+</html>
